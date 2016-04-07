487b71a29d5fc82bfca2aaf1c6ed98da093530aa
Estefanio NS <estefanions@gmail.com>
2016-04-06 21:46:00 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# ZuniCSS
+```html
+<!DOCTYPE HTML>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="css/style.min.css" type="text/css" media="all" />
+        <title>Zuni CSS</title>
+    </head>
+<body>
+
+    <!-- content here -->
+
+    <script type="text/javascript" src="https://code.jquery.com/jquery-latest.js"></script>
+    <script type="text/javascript" src="js/script.js"></script>
+<!--[if lt IE 9]>
+    <script src="https://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+</body>
+</html>
+```
---
+++ js/script.js
@@ -0,0 +1,105 @@
+/*
+*
+* ------------------------------------------------------
+* script for ZuniCSS 
+* ------------------------------------------------------
+*
+* Estefanio NS <estefanions AT gmail DOT com 
+* project: https://github.com/4app/zunicss
+* page:    http://4app.github.io/zunicss
+*/
+
+$( document ).ready(function() {
+$('.menu_bar').prepend('<div class="menu_bt">Menu</div>');
+	$('.menu_bar .menu_bt').on('click', function(){
+		var menu = $(this).next('ul');
+		if (menu.hasClass('open')) {
+			menu.removeClass('open');
+		}
+		else {
+			menu.addClass('open');
+		}
+	});
+
+$('.menu_sidebar li.has_dropdown>a').on('click', function(){
+		$(this).removeAttr('href');
+		var element = $(this).parent('li');
+		if (element.hasClass('open')) {
+			element.removeClass('open');
+			element.find('li').removeClass('open');
+			element.find('ul').slideUp();
+		}
+		else {
+			element.addClass('open');
+			element.children('ul').slideDown();
+			element.siblings('li').children('ul').slideUp();
+			element.siblings('li').removeClass('open');
+			element.siblings('li').find('li').removeClass('open');
+			element.siblings('li').find('ul').slideUp();
+		}
+	});
+	$('.menu_sidebar>ul>li.has_dropdown>a').append('<span class="holder"></span>');
+
+	(function getColor() {
+		var r, g, b;
+		var textColor = $('.menu_sidebar').css('color');
+		textColor = textColor.slice(4);
+		r = textColor.slice(0, textColor.indexOf(','));
+		textColor = textColor.slice(textColor.indexOf(' ') + 1);
+		g = textColor.slice(0, textColor.indexOf(','));
+		textColor = textColor.slice(textColor.indexOf(' ') + 1);
+		b = textColor.slice(0, textColor.indexOf(')'));
+		var l = rgbToHsl(r, g, b);
+		if (l > 0.7) {
+			$('.menu_sidebar>ul>li>a>span').css('border_color', 'rgba(0, 0, 0, .35)');
+		}
+		else
+		{
+			$('.menu_sidebar>ul>li>a>span').css('border_color', 'rgba(255, 255, 255, .35)');
+		}
+	})();
+
+	function rgbToHsl(r, g, b) {
+	    r /= 255, g /= 255, b /= 255;
+	    var max = Math.max(r, g, b), min = Math.min(r, g, b);
+	    var h, s, l = (max + min) / 2;
+
+	    if(max == min){
+	        h = s = 0;
+	    }
+	    else {
+	        var d = max - min;
+	        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+	        switch(max){
+	            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+	            case g: h = (b - r) / d + 2; break;
+	            case b: h = (r - g) / d + 4; break;
+	        }
+	        h /= 6;
+	    }
+	    return l;
+	}
+
+
+var metas = document.getElementsByTagName('meta');
+var i;
+if (navigator.userAgent.match(/iPhone/i)) {
+  for (i=0; i<metas.length; i++) {
+    if (metas[i].name == "viewport") {
+      metas[i].content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
+    }
+  }
+  document.addEventListener("gesturestart", gestureStart, false);
+}
+function gestureStart() {
+  for (i=0; i<metas.length; i++) {
+    if (metas[i].name == "viewport") {
+      metas[i].content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6";
+    }
+  }
+}
+
+
+
+});
+
